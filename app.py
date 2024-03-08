aa29951a1ea1855c897cde0c6b70eaaf2b6c7716
cuongnp3110 <cuongnp3110@gmail.com>
2024-03-09 04:35:28 +0700

    Update port

--- app.py
+++ app.py
@@ -180,5 +180,8 @@ class predictApi(Resource):
 
 api.add_resource(predictApi, '/api/predict')
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+    
